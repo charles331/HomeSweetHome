49f2aed5a781397730ece05128a78d28574f61fd
Charles Meunier <charles.meunier@speos.be>
2018-05-07 15:45:18 +0200

    see & delete one human by ID

--- HomeSweetHome/src/main/java/be/home/sweet/control/HumanController.java
+++ HomeSweetHome/src/main/java/be/home/sweet/control/HumanController.java
@@ -1,11 +1,14 @@
 package be.home.sweet.control;
 
+import java.util.Optional;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -50,10 +53,31 @@ public class HumanController {
 			return mav;			
 		}
 		System.out.println("save human " + human.getFirstName());
-		humanRepo.save(human);
+		humanRepo.save(human);		
 		mav.setViewName("allhumans");
 		mav.addObject("humans", humanRepo.findAll());
 		return mav;
 	}
+	
+	//https://stackoverflow.com/questions/33498114/spring-data-mvc-thymeleaf-update-entity
+	//https://medium.com/@grokwich/spring-boot-thymeleaf-html-form-handling-762ef0d51327
+	@GetMapping("human-details/{humanId}")
+	public ModelAndView seeHumanDetails(@PathVariable("humanId") Long id) {
+		ModelAndView mav = new ModelAndView();		
+		Optional<Human> hm = humanRepo.findById(id);
+		mav.setViewName("human-details");
+		mav.addObject("human", hm);
+		return mav;
+	}
+	
+	@GetMapping("human-delete/{humanId}")
+	public ModelAndView deleteHumanDetails(@PathVariable("humanId") Long id) {
+		ModelAndView mav = new ModelAndView();			
+		humanRepo.deleteById(id);
+		mav.setViewName("allhumans");
+		mav.addObject("humans", humanRepo.findAll());
+		return mav;
+	}
+	
 	
 }
